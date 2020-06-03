ad177ca4794f22ccf3b6b45234d164ddaef99d45
Sergio Ortiz <ortizsergio1234@gmail.com>
2020-06-03 18:12:16 +0000

    Cleans code by removing unused function, renaming function

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -13,24 +13,9 @@
 // limitations under the License.
 
 /**
- * Adds a random greeting to the page.
+ * Fetches comments from the servers and adds them to the DOM.
  */
-function addRandomGreeting() {
-  const greetings =
-      ['Hello world!', '¡Hola Mundo!', '你好，世界！', 'Bonjour le monde!'];
-
-
-  // Pick a random greeting.
-  const greeting = greetings[Math.floor(Math.random() * greetings.length)];
-
-  // Add it to the page.
-  const greetingContainer = document.getElementById('greeting-container');
-  greetingContainer.innerText = greeting;
-}
-
-
-
-function getServerStats() {
+function getServerComments() {
     fetch('/data')
     .then(response => response.json())
     .then((myObject) => {
